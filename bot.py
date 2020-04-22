e9e484fdafdfbfa2d7fb5df4bd1791a91e3a3312
Siddharth Rathore <41838184+SidtehDuck@users.noreply.github.com>
2020-04-23 00:17:45 +0530

    Cleared old stuff

--- bot.py
+++ bot.py
@@ -1,17 +1,12 @@
-import discord
-import json
-import os
-import time
-import random
+import discord, os, time, random, json
 from discord.utils import get
 from discord.ext import commands
 from discord import Embed, Emoji
 from discord.ext.commands import Bot
 
-TOKEN = 'NzAxNTA4NjAxODY0MjU3NjM5.Xpyg8g.00VMNpm9Uen5H73itaNVg7eskZA'
+TOKEN = 'redacted'
 client = commands.Bot(command_prefix = '.cin ')
-os.chdir(r'C:\Users\chiku\Downloads\COVID-IN')
-
+os.chdir(r'directory here')
 
 @client.event
 async def on_ready():
@@ -19,13 +14,7 @@ async def on_ready():
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'your retarded ass is delayed by {round(client.latency * 1000)} ms')
-
-@client.command()
-async def harshit(ctx):
-
-    for i in range(1):
-        await ctx.send('atti batti shatti harshit khaye batti <@553560674043953163>')
+    await ctx.send(f'You are delayed by {round(client.latency * 1000)} ms')
 
 
 client.run(TOKEN)
