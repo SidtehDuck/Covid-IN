1acc30902f20c24a89b552d4c66174756f1359f5
SidtehDuck <chiku4always@gmail.com>
2020-05-01 03:15:20 +0530

    figured out json

--- bot.py
+++ bot.py
@@ -5,11 +5,27 @@ from discord import Embed, Emoji
 from discord.ext.commands import Bot
 import requests
 
-TOKEN = 'redacted'
-client = commands.Bot(command_prefix = '.cin ')
-#os.chdir(r'directory here')
+#holy fucking shit this code is spaghetti as fuck
 
-response = requests.get('')
+r = requests.get('https://api.covid19india.org/data.json')
+data = r.json()
+
+print('Total cases so far in India: ' + data['statewise'][0]['confirmed'] + '\n')
+
+if data['statewise'][1]['statecode'] == 'MH':
+    print('YEET MAHARASHTRA IS FUCKED BOIS')
+    print('Confirmed cases: ' + data['statewise'][1]['confirmed'])
+    print('Active cases: ' + data['statewise'][1]['active'])
+
+if data['statewise'][2]['statecode'] == 'GJ':
+    print('GUJARAT IS FUCKING DYING TOO BOIS')
+    print('Confirmed cases: ' + data['statewise'][2]['confirmed'])
+    print('Active cases: ' + data['statewise'][2]['active'])
+
+
+TOKEN = 'redacted'
+client = commands.Bot(command_prefix = '%')
+os.chdir(r'home/ducc/duccz/Covid-IN')
 
 @client.event
 async def on_ready():
--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import requests, json, time, os, random
+import pandas as pd, numpy as np, matplotlib as plt
 
-r = requests.get('https://api.covid19india.org/raw_data.json')
 
-print(r.status_code)
+r = requests.get('https://api.covid19india.org/data.json')
 
-data = r.text
+data = r.json()
 
-print(data)
+print('Total cases so far in India: ' + data['statewise'][0]['confirmed'] + '\n')
+
+
+if data['statewise'][1]['statecode'] == 'MH':
+    print('YEET MAHARASHTRA IS FUCKED BOIS')
+    print('Confirmed cases: ' + data['statewise'][1]['confirmed'])
+    print('Active cases: ' + data['statewise'][1]['active'])
+
+if data['statewise'][2]['statecode'] == 'GJ':
+    print('GUJARAT IS FUCKING DYING TOO BOIS')
+    print('Confirmed cases: ' + data['statewise'][2]['confirmed'])
+    print('Active cases: ' + data['statewise'][2]['active'])
