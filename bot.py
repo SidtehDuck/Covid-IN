a58b4a0973f151d96002d707140b9da0bbeaa020
SidtehDuck <chiku4always@gmail.com>
2020-05-02 22:51:46 +0530

    achieved basic functionality

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ data = r.json()
 
 print('Total cases so far in India: ' + data['statewise'][0]['confirmed'] + '\n')
 
-if data['statewise'][1]['statecode'] == 'MH':
+'''if data['statewise'][1]['statecode'] == 'MH':
     print('YEET MAHARASHTRA IS FUCKED BOIS')
     print('Confirmed cases: ' + data['statewise'][1]['confirmed'])
     print('Active cases: ' + data['statewise'][1]['active'])
@@ -24,10 +24,20 @@ if data['statewise'][2]['statecode'] == 'GJ':
 
 token_file = open('token.json',)
 token_data = json.load(token_file)
-TOKEN = token_file['token']
+TOKEN = str(token_file['token'])
+'''
+
+with open("token.json", "r") as infile:
+    try:
+        CONFIG = json.load(infile)
+        TOKEN = CONFIG["token"]
+    except (KeyError, FileNotFoundError):
+        raise EnvironmentError(
+            "Your config.json file is either missing, or incomplete. Check your config.json and ensure it has the keys 'token' and 'owner_id'"
+        )
 
 client = commands.Bot(command_prefix = '%')
-os.chdir(r'home/ducc/duccz/Covid-IN')
+#os.chdir(r'home/ducc/duccz/Covid-IN')
 
 @client.event
 async def on_ready():
@@ -37,5 +47,30 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f'You are delayed by {round(client.latency * 1000)} ms')
 
+@client.command()
+async def statMH(ctx):
+    await ctx.send('cases in maharashtra rn: ' + data['statewise'][1]['confirmed'])
+
+@client.command()
+async def statPB(ctx):
+    await ctx.send('confirmed cases in punjab rn: ' + data['statewise'][15]['confirmed'])
+    await ctx.send('active cases in punjab rn: ' + data['statewise'][15]['active'])
+    await ctx.send('total deaths in punjab rn: ' + data['statewise'][15]['deaths'])
+
+@client.command()
+async def statCG(ctx):
+    await ctx.send('confirmed cases in CG rn: ' + data['statewise'][23]['confirmed'])
+    await ctx.send('active cases in CG rn: ' + data['statewise'][23]['active'])
+    await ctx.send('total deaths in CG rn: ' + data['statewise'][23]['deaths'])
+
+@client.command()
+async def statRJ(ctx):
+    await ctx.send('confirmed cases in RJ rn: ' + data['statewise'][5]['confirmed'])
+    await ctx.send('active cases in RJ rn: ' + data['statewise'][5]['active'])
+    await ctx.send('total deaths in RJ rn: ' + data['statewise'][5]['deaths'])
+
+@client.command()
+async def statlemi(ctx):
+    await ctx.send('LEMI PUT MY NUTS ON YOUR FACE LOL')
 
 client.run(TOKEN)
